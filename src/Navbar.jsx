3582b463ed07403e0534e7b5e5bf89b2c9962fa3
Emre OZENSES <emreozenses@gmail.com>
2024-05-16 17:17:10 +0300

    navbar elements and css completed

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import sublinks from './data'
+import { useGlobalContext } from './context'
+
+
 
 const Navbar = () => {
+
+    const {isSidebarOpen,setSidebarOpen,OpenSidebar,CloseSidebar} = useGlobalContext();
+
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <div className="nav-links">
+         
+            {sublinks.map((mainLinks) => {
+              const { pageId, page, links } = mainLinks
+              return <button className='nav-link' key={pageId}>{page}</button>
+            })}
+          
+        </div>
+        <button 
+        type='button'
+        className="toggle-btn"
+        onClick={OpenSidebar}>
+          <FaBars/>
+        </button>
+      </div>
+    </nav>
   )
 }
 
