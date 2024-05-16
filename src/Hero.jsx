ed965c2048ac4121a1e5852c702433b9a55254b5
Emre OZENSES <emreozenses@gmail.com>
2024-05-16 18:31:43 +0300

    Hero component and css completed

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>Manage Any Content Anywhere</h1>
+        <p>
+          Strapi is the leading open-source headless CMS. It’s 100% JavaScript
+          and fully customizable.
+        </p>
+      </div>
+    </div>
   )
 }
 
